371e54d5f12063d15b6bae60e3b5d29063729500
Bravo1B9 <b1@xgmi.com>
2022-12-12 05:48:16 +0000

    Update create post

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,50 +1,37 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 import CreatePostDto from './dto/createPost.dto';
 import UpdatePostDto from "./dto/updatePost.dto";
-import Post from "./post.interface";
+import Post from "./post.entity";
 
 @Injectable()
 export default class PostsService {
-  private lastPostId = 0;
-  private posts: Post[] = [];
-
-  createPost(post: CreatePostDto) {
-    const newPost = {
-      id: ++this.lastPostId,
-      ...post
-    }
-    this.posts.push(newPost);
+  constructor (
+    @InjectRepository(Post)
+    private postsRepository: Repository<Post>
+  ) {}
+  
+  async createPost(post: CreatePostDto) {
+    const newPost = await this.postsRepository.create(post);
+    await this.postsRepository.save(newPost);
     return newPost;
   }
 
   getPostById(id: number) {
-    const post = this.posts.find(post => post.id === id);
-    if (post) {
-      return post;
-    }
-    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+    
   }
 
   getAllPosts() {
-    return this.posts;
+    
   }
 
   replacePost(id: number, post: UpdatePostDto) {
-    const postIndex = this.posts.findIndex(post => post.id === id);
-    if (postIndex > -1) {
-      this.posts[postIndex] = post;
-      return post;
-    }
-    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+    
   }
 
   deletePost(id: number) {
-    const postIndex = this.posts.findIndex(post => post.id === id);
-    if (postIndex > -1) {
-      this.posts.splice(postIndex, 1);
-    } else {
-      throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
-    }
+    
   }
 
 }
