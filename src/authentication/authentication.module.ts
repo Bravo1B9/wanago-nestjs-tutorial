e56bef579a51e3a06316339660c662fa67edca13
Bravo1B9 <b1@xgmi.com>
2022-12-22 12:27:03 +0000

    Implement local strategy

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
+import { PassportModule } from "@nestjs/passport";
 import { UsersModule } from "src/users/users.module";
 import { AuthenticationController } from "./authentication.controller";
 import { AuthenticationService } from "./authentication.service";
+import { LocalStrategy } from "./local.strategy";
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PassportModule],
+  providers: [AuthenticationService, LocalStrategy],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
 })
 export class AuthenticationModule {}
